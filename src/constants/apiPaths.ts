a7508c00f91a5abf4d36ec875e74f3e9a195e04e
Racharla Sanjana <racharla_sanjana@epam.com>
2026-05-06 18:19:03 +0530

    Changes made for DynamoDB Task

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: import.meta.env.VITE_PRODUCT_SERVICE_URL || "https://vwlykfdzq8.execute-api.us-east-1.amazonaws.com/prod",
+  product: import.meta.env.VITE_PRODUCT_SERVICE_URL || "https://YOUR_API_ID.execute-api.ap-south-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
