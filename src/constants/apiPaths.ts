23a92dc7dc882869819d93ac60fa80f740b561f9
Racharla Sanjana <racharla_sanjana@epam.com>
2026-05-10 17:13:23 +0530

    Uploding csv file

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: import.meta.env.VITE_PRODUCT_SERVICE_URL || "https://YOUR_API_ID.execute-api.ap-south-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: import.meta.env.VITE_IMPORT_SERVICE_URL || "https://YOUR_IMPORT_API_ID.execute-api.ap-south-1.amazonaws.com/prod",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
